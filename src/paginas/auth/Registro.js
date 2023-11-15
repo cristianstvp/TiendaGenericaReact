f01351e25d46f3bc0db49c9abfcd9aede103af15
cristianstvp <88211337+cristianstvp@users.noreply.github.com>
2023-11-15 14:41:33 -0500

    vid8. Sistema de Registro actualizable

--- src/paginas/auth/Registro.js
+++ src/paginas/auth/Registro.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from 'react-router-dom';
 
 
 const Registro = () => {
+  const [Usuario, setUsuario] = useState({
+    idTipoDocumento : '',
+    numeroDocumento : '',
+    email : '',
+    nombre :'',
+    password : '',
+    nombreUsuario : '',
+    confirmar:''
+  })
+
+  const {idTipoDocumento,numeroDocumento,email,nombre,password,confirmar,nombreUsuario} = Usuario;
+
+  const onChange = (e) =>{
+    setUsuario(e.target.value);
+
+    setUsuario({ ...Usuario, [e.target.name]:e.target.value});
+  }
+
   return (
     <div className="hold-transition register-page">
       <div className="register-box">
@@ -19,49 +37,92 @@ const Registro = () => {
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Full name"
+                  placeholder="Nombre completo"
+                  id="nombre"
+                  name="nombre"
+                  value={nombre}
+                  onChange={onChange}
+                  required
                 />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-user" />
-                  </div>
-                </div>
               </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre de Usuario"
+                  id="nombreUsuario"
+                  name="nombreUsuario"
+                  value={nombreUsuario}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+
+              <div className="input-group mb-3">
+                <select
+                  className="form-control"
+                  value={idTipoDocumento}
+                  id="idTipoDocumento"
+                  name="idTipoDocumento"
+                  onChange={onChange}
+                  required
+                  >
+                  <option value="">Selecciona un tipo de documento</option>
+                  <option value="5">Cédula</option>
+                  <option value="6">Tarjeta</option>
+                  <option value="7">Pasaporte</option>
+                  </select>
+              </div>
+
+              <div className="input-group mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Numero de documento"
+                  id="numeroDocumento"
+                  name="numeroDocumento"
+                  value={numeroDocumento}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+
               <div className="input-group mb-3">
                 <input
                   type="email"
                   className="form-control"
                   placeholder="Email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                  required
                 />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-envelope" />
-                  </div>
-                </div>
+              </div>
+
+              <div className="input-group mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Contraseña"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                  required
+                />
               </div>
               <div className="input-group mb-3">
                 <input
                   type="password"
                   className="form-control"
-                  placeholder="Password"
+                  placeholder="Repite tu contraseña"
+                  id="confirmar"
+                  name="confirmar"
+                  value={confirmar}
+                  onChange={onChange}
+                  required
                 />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-lock" />
-                  </div>
-                </div>
-              </div>
-              <div className="input-group mb-3">
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Retype password"
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-lock" />
-                  </div>
-                </div>
               </div>
               <div className="row">
                 <div className="col-8">
