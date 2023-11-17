1653e3097641125e2dc02528e9f47a39d6322d02
cristianstvp <88211337+cristianstvp@users.noreply.github.com>
2023-11-16 22:27:15 -0500

    Video14 inicio de Menu post logueo

---
+++ src/componentes/Menu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <nav className="mt-2">
+      <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li className="nav-item">
+          <a href="../widgets.html" className="nav-link">
+            <i className="nav-icon fas fa-th" />
+            <p>
+              Widgets
+            </p>
+          </a>
+        </li>
+    </ul>
+    </nav>
+  );
+};
+
+export default Menu;
--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../componentes/Navbar'
+import SidebarContainer from '../componentes/SidebarContainer';
 
-const Home=  () => {
-     return (
-        <div>Home</div>
+
+const Home = () => {
+   return (
+<div className="wrapper">
+   <Navbar></Navbar>
+   <SidebarContainer></SidebarContainer>
+</div>
+
      )
 }
 
